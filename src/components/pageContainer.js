332c2852afb6252c4190d2764bd5da52c957ea66
kasey purvor <kasey.purvor@gmail.com>
2023-07-27 11:24:25 +0100

    pagecontainer m-size-auto change

--- src/components/pageContainer.js
+++ src/components/pageContainer.js
@@ -1,6 +1,6 @@
 export default function PageContainer({children}) {
     return (
-        <main className=' bg-gray-900 min-h-screen  flex flex-col items-center min-py-6 px-2 sm:p-1 rounded-3xl m-20 sm:m-5 '>
+        <main className=' bg-gray-900 min-h-auto  flex flex-col items-center min-py-6 px-2 sm:p-1 rounded-3xl m-20 sm:m-5 '>
             {children}
         </main>
     )
